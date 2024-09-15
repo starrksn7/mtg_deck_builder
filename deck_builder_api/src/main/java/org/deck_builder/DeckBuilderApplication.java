b9bfbc0f5d72f528e65a31f2819c1ec9c17cd0b4
Robert Weeks <rpweeks_2@yahoo.com>
2024-09-15 17:08:08 -0400

    final updates made.  Need to establish db connection.

---
+++ deck_builder_api/src/main/java/org/deck_builder/DeckBuilderApplication.java
@@ -0,0 +1,14 @@
+package org.deck_builder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
+public class DeckBuilderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeckBuilderApplication.class, args);
+    }
+
+}
