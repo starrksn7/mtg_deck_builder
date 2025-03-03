b139cf65465946b443ce1e3d8e734561567738bf
starrksn7 <rpweeks_2@yahoo.com>
2025-03-03 18:28:20 -0500

    getting started on a page for the decks

--- deck_builder_ui/src/App.js
+++ deck_builder_ui/src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import { SearchBar } from './search/searchBar';
 import 'mana-font/css/mana.css'
-
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <div>Whatever I want goes here</div>
       <SearchBar />
+      <Routes>
+        <Route path="/decks" element={<AllDecks />} />
+      </Routes>
     </div>
   );
 }
