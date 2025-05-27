022e75e88e4b9640ffa209f8ed3025d90352db55
starrksn7 <rpweeks_2@yahoo.com>
2025-05-27 17:54:26 -0400

    working on search for create commander page

--- deck_builder_ui/src/decks/createDeck.js
+++ deck_builder_ui/src/decks/createDeck.js
@@ -6,16 +6,29 @@ import { getBaseColors } from "../helperFunctions"
 export function CreateDeck(){
     const [searchColor, setSearchColor] = useState('')
     const [searchInput, setSearchInput] = useState('')
+    const [searchResults, setSearchResults] = useState([])
 
     const findCommanderWithSearch = async () => {
         const response = await axios.post('http://localhost:8080/card/searchForCommander', 
             {keyword: searchInput})
+        
+        let resultsArray = [];
+        let data = response.data;
+        data.forEach(entry => resultsArray.push(JSON.parse(entry)))
+
+        setSearchResults(resultsArray);
     }
 
     const findCommanderByColor = async () => {
         const response = await axios.post('http://localhost:8080/card/findCommanderByColors',
             {colorIdentity: searchColor}
         )
+
+        let resultsArray = [];
+        let data = response.data;
+        data.forEach(entry => resultsArray.push(JSON.parse(entry)))
+
+        setSearchResults(resultsArray);
     }
 
     const handleChange = (e) => {
