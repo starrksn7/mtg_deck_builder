4be168f5daee211e1a473b88b5d73c08df2e67c4
starrksn7 <rpweeks_2@yahoo.com>
2025-06-19 14:40:09 -0400

    working on create page options

--- deck_builder_ui/src/search/displayResults.js
+++ deck_builder_ui/src/search/displayResults.js
@@ -1,11 +1,14 @@
 import React from "react"
 import axios from 'axios'
 import { replaceTextWithManaSymbols, createCardObject } from "../helperFunctions"
+import { useLocation } from "react-router-dom"
 
 
 export const DisplayResults = ({searchResults, deckId, setIsError}) => {
     console.log("searchResults in displayResults = ")
     console.log(searchResults)
+    const location = useLocation();
+    const isOnCreatePage = location.pathname === '/create' 
 
     const addToDeck = async (card) => {
         let cardObject = createCardObject(card)
@@ -13,6 +16,14 @@ export const DisplayResults = ({searchResults, deckId, setIsError}) => {
         const res = await axios.post('http://localhost:8080/decks/add', { deckId: 1, cardDto: cardObject})
         if(res) console.log("added card to deck. Need to find a better notification than this")
         else console.log("Couldn't add the card to deck, for some reason")
+   }
+
+   const setAsCommander = async (card) => {
+        let cardObject = createCardObject(card)
+
+        const res = await axios.post('http://localhost:8080/decks/create', { userId: 1, deckName, cardObject})
+        if(res) console.log("deck created")
+        else console.log("new deck could not be created")
    }
 
     if(searchResults[0]?.error) {
@@ -33,7 +44,9 @@ export const DisplayResults = ({searchResults, deckId, setIsError}) => {
                     <div>Type: {card.type}</div>
                     <div dangerouslySetInnerHTML={{ __html: replaceTextWithManaSymbols(card.oracle_text) }} ></div>
                     {/* <form onSubmit={addToDeck}> */}
-                        <button type="submit" onClick={() => addToDeck(card)}>Add to Deck</button>
+                        <button type="submit" onClick={() => isOnCreatePage ? setAsCommander(card) : addToDeck(card)}>
+                            {isOnCreatePage ? 'Set as Commander' : 'Add to Deck'}
+                        </button>
                     {/* </form> */}
                 </div>  
             </div>
