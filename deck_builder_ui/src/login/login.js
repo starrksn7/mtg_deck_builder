171aafeb9ced94660591dbaf29aa80fbfefd6b32
starrksn7 <rpweeks_2@yahoo.com>
2025-08-13 19:48:03 -0400

    getting started on login component

---
+++ deck_builder_ui/src/login/login.js
@@ -0,0 +1,17 @@
+export function Login() {
+    return (
+        <div>
+            <form>
+                <input 
+                    type="text"
+                    placeholder="Username" 
+                />
+                <input
+                    type="text"
+                    placeholder="Password" 
+                />
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    )
+}
