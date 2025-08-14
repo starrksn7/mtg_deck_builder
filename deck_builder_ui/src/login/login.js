cf438f0874c4196e79d7370dd96ce7a4e8ebf081
starrksn7 <rpweeks_2@yahoo.com>
2025-08-14 19:40:02 -0400

    working on login functions for the ui

--- deck_builder_ui/src/login/login.js
+++ deck_builder_ui/src/login/login.js
@@ -1,16 +1,47 @@
+import { useState } from "react"
+import axios from "axios";
+
 export function Login() {
+    const [username, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState(null);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        try{ 
+            const response = axios.post('http://localhost:8080/user/login', {
+                username,
+                password
+            })
+
+            const token = response.data
+
+            localStorage.setItem('jwt', token)
+            console.log('Login successful.')
+        } catch (err){
+            setError('Login failed.')
+            console.error(err)
+        }
+    }
+
     return (
         <div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input 
                     type="text"
+                    value={username}
+                    onChange={(e) => setUserName(e.target.value)}
                     placeholder="Username" 
                 />
                 <input
                     type="text"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
                     placeholder="Password" 
                 />
                 <button type="submit">Submit</button>
+                {error && <p style={{ color: 'red' }}>{error}</p>}
             </form>
         </div>
     )
