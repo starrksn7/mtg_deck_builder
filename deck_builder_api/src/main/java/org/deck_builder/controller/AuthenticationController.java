f5081287ca58a5d424414cceb524944fc9177b51
starrksn7 <rpweeks_2@yahoo.com>
2025-08-19 20:10:24 -0400

    working on authorization controller

--- deck_builder_api/src/main/java/org/deck_builder/controller/AuthenticationController.java
+++ deck_builder_api/src/main/java/org/deck_builder/controller/AuthenticationController.java
@@ -1,9 +1,22 @@
 package org.deck_builder.controller;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.Valid;
 import org.deck_builder.dao.UserDao;
+import org.deck_builder.model.LoginDTO;
+import org.deck_builder.model.User;
+import org.deck_builder.security.jwt.JWTFilter;
 import org.deck_builder.security.jwt.TokenProvider;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,5 +31,52 @@ class AuthenticationController {
         this.tokenProvider = tokenProvider;
         this.authenticationManagerBuilder = authenticationManagerBuilder;
         this.userDao = userDao;
+    }
+
+    @PostMapping(value = "/login")
+    public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginDTO loginDto) {
+
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(loginDto.getEmail(), loginDto.getPassword());
+
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = tokenProvider.createToken(authentication, false);
+
+        User user = userDao.findUserByEmail(loginDto.getEmail());
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(JWTFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
+        return new ResponseEntity<>(new LoginResponse(jwt, user), httpHeaders, HttpStatus.OK);
+    }
+
+
+    static class LoginResponse {
+
+        private String token;
+        private User user;
+
+        LoginResponse(String token, User user) {
+            this.token = token;
+            this.user = user;
+        }
+
+        @JsonProperty("token")
+        String getToken() {
+            return token;
+        }
+
+        void setToken(String token) {
+            this.token = token;
+        }
+
+        @JsonProperty("user")
+        public User getUser() {
+            return user;
+        }
+
+        public void setUser(User user) {
+            this.user = user;
+        }
     }
 }
