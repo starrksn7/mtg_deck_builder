9b71478e7a82f7dadc7a5b8c0f007565540e2fc1
starrksn7 <rpweeks_2@yahoo.com>
2025-09-07 13:55:45 -0400

    getting register component set up in the ui

---
+++ deck_builder_ui/src/login/register.js
@@ -0,0 +1,63 @@
+import { useState } from "react"
+import axios from "axios";
+
+export function Login() {
+    const [username, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [email, setEmail] = useState('')
+    const [error, setError] = useState(null);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        try{ 
+            const response = axios.post('http://localhost:8080/register', {
+                email,
+                username,
+                password
+            })
+
+            const token = response.data
+
+            localStorage.setItem('jwt', token)
+            console.log('Login successful.')
+        } catch (err){
+            setError('Login failed.')
+            console.error(err)
+        }
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUserName(e.target.value)}
+                    placeholder="Username" 
+                />
+                <input
+                    type="text"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Password" 
+                />
+                <input
+                    type="text"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Password" 
+                />
+                <input
+                    type="text"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Password" 
+                />
+                <button type="submit">Submit</button>
+                {error && <p style={{ color: 'red' }}>{error}</p>}
+            </form>
+        </div>
+    )
+}
