da99090a45479afd7b4760d2f3b5453b0519c161
starrksn7 <rpweeks_2@yahoo.com>
2025-12-14 19:34:12 -0500

    working on header

--- deck_builder_ui/src/header/header.js
+++ deck_builder_ui/src/header/header.js
@@ -1,13 +1,26 @@
 export const Header = () => {
     const userId = localStorage.getItem('userId');
     const token = localStorage.getItem('jwt');
+    
+    const handleLogout = () => {
+        localStorage.removeItem("accessToken");
+        localStorage.removeItem("user");
+        localStorage.removeItem("userId");
+    }
 
+    if (token){
+        return (
+            <div>
+                <div onClick={handleLogout}>Logout</div>
+                <a href={`/user/${userId}`}>My Decks</a>
+                <a href="/create">Create</a>
+            </div>
+        )
+    }
 
     return (
         <div>
             <a href="/login">Login</a>
-            <a href={`/user/${userId}`}>My Decks</a>
-            <a href="/create">Create</a>
         </div>
     )
 }
