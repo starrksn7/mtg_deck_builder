8231b7bde146469ea6c964a244e75a5b4b74146b
starrksn7 <rpweeks_2@yahoo.com>
2025-12-19 10:28:30 -0500

    came up with a plan to keep results from being added to a deck when it's not a legal commander card

--- deck_builder_api/src/main/java/org/deck_builder/DeckBuilderApplication.java
+++ deck_builder_api/src/main/java/org/deck_builder/DeckBuilderApplication.java
@@ -19,7 +19,10 @@ public class DeckBuilderApplication {
  *    accept the quantity of the card instead of making the user list 35 basic lands or something like that.
  *    This might still make it necessary to add the cards to the database separately, but then it might also mean
  *    that I would need to change the delete logic to check if something is there more than once?
- *    a. add protection to keep people from being able to put in specialty cards/tokens
+ * ----> a. add protection to keep people from being able to put in specialty cards/tokens
+ *          i. need to add a legalities array of objects to the CardSearchDTO
+ *          ii. need to pull that value from the scryfall collection results and apply it when mapping card to CardSearchDTO
+ *          iii. need to then check legalities for the "commander" value and if it isn't legal, then filter that card out
  * 3. Some kind of initial landing page would be nice, not sure what that would look like, can I make a page of
  *    dummy articles/images?
  */
