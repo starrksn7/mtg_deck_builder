29924dba9a72f0f95e1779ccf28777a965226b49
starrksn7 <rpweeks_2@yahoo.com>
2026-05-18 19:20:53 -0400

    fixed logout button appearing behind banner in the single deck page

--- deck_builder_api/src/main/java/org/deck_builder/DeckBuilderApplication.java
+++ deck_builder_api/src/main/java/org/deck_builder/DeckBuilderApplication.java
@@ -15,10 +15,12 @@ public class DeckBuilderApplication {
 
 /** Tasks to complete
  * 1. Style the pages
- *    a. the logout dropdown falls behind the banner on the single deck page
+ *    a. Still need to sort out the single deck card display. Current issue is that the category is being counted as a
+ *       card, so one card splits the category into one column and the card on the other side.
  * 2. Some kind of initial landing page would be nice, not sure what that would look like, can I make a page of
  *    dummy articles/images?
  *    a. Right now just using the user's deck page as the initial page after logging in
  * 3. Maybe change the image of the commander card for a deck in the deck page? Or add the full art image and use that
  *    instead of the normal card image.
+ * 4. full art link not saving correctly when setting a card as commander
  */
