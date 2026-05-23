bc649dded0922e4f5be7b9502c4990689f51f653
starrksn7 <rpweeks_2@yahoo.com>
2026-05-23 12:38:44 -0400

    working on making the loading spinner reflect the mana symbols

--- deck_builder_api/src/main/java/org/deck_builder/DeckBuilderApplication.java
+++ deck_builder_api/src/main/java/org/deck_builder/DeckBuilderApplication.java
@@ -14,12 +14,10 @@ public class DeckBuilderApplication {
 }
 
 /** Tasks to complete
- * 1. Style the pages
- *    a. Still need to sort out the single deck card display. Current issue is that the category is being counted as a
- *       card, so one card splits the category into one column and the card on the other side.
- * 2. Some kind of initial landing page would be nice, not sure what that would look like, can I make a page of
+ * 1. Some kind of initial landing page would be nice, not sure what that would look like, can I make a page of
  *    dummy articles/images?
- *    a. Right now just using the user's deck page as the initial page after logging in
- * 3. Maybe change the image of the commander card for a deck in the deck page? Or add the full art image and use that
+ * 2. Maybe change the image of the commander card for a deck in the deck page? Or add the full art image and use that
  *    instead of the normal card image.
+ * 3. Can I make a custom loading spinner that uses the five color symbols?
+ * 4. Should probably change the style of the page to accommodate different screen sizes
  */
--- deck_builder_ui/src/search/loader.js
+++ deck_builder_ui/src/search/loader.js
@@ -1,13 +1,17 @@
-import { RotatingLines } from "react-loader-spinner";
+import "../App.css";
 
 export function Loader() {
-    return (
-      <RotatingLines
-        strokeColor="grey"
-        strokeWidth="5"
-        animationDuration="0.75"
-        width="96"
-        visible={true}
-      />
-    )
-  }
+  const symbols = ["w", "u", "b", "r", "g", "c"];
+
+  return (
+    <div className="mana-spinner">
+      {symbols.map((symbol, index) => (
+        <span
+          key={symbol}
+          className={`ms ms-${symbol} mana-symbol symbol-${index}`}
+          // className={`<i class="ms ms-${symbol} ms-cost ms-shadow"></i>`}
+        />
+      ))}
+    </div>
+  );
+}
