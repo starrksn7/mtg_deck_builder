9866d783129fb1f3e02ea57d243b2782b9f146f9
starrksn7 <rpweeks_2@yahoo.com>
2026-06-01 19:52:06 -0400

    updating to do list

--- deck_builder_api/src/main/java/org/deck_builder/DeckBuilderApplication.java
+++ deck_builder_api/src/main/java/org/deck_builder/DeckBuilderApplication.java
@@ -17,7 +17,9 @@ public class DeckBuilderApplication {
  * 1. Some kind of initial landing page would be nice, not sure what that would look like, can I make a page of
  *    dummy articles/images?
  * 2. Should probably change the style of the page to accommodate different screen sizes
- * 3. delete deck button isn't working
- * 4. Need to figure out a way to incorporate partner/background/friends forever and all that similar stuff into
+ * 3. Need to figure out a way to incorporate partner/background/friends forever and all that similar stuff into
  *    the results search for a commander
+ *    a. background cards already show up in the search
+ *    b. partner does the same thing
+ * 4. Need to check out the error handling for when no search results were found. Currently getting an error
  */
