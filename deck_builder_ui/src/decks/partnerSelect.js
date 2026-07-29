d32b4b453b45cf4075a35fdcca3918ec5ebb6136
starrksn7 <rpweeks_2@yahoo.com>
2026-07-29 19:09:43 -0400

    working on partner select

--- deck_builder_ui/src/decks/partnerSelect.js
+++ deck_builder_ui/src/decks/partnerSelect.js
@@ -5,6 +5,7 @@ import { useState, useEffect } from "react";
 
 export const PartnerSelect = () => {
     //once a partner is selected I need to make sure I update the deck information with the partnerId and the colorIdentity
+    //as well as add the card to the decklist
     const [isLoading, setIsLoading] = useState(false);
     const [optionsNotFound, setOptionsNotFound] = useState(false);
     const [cardList, setCardList] = useState([]);
@@ -37,6 +38,33 @@ export const PartnerSelect = () => {
     return (
         <div>
             {isLoading && <Loader />}
+
+            <div className="results-grid">
+                {cardList.map((card) => (
+                    <div className="result-row">
+                        <img src={card.fullArtLink} alt={card.name} />
+                        <div className="content">
+                            <div className="top">
+                                <div className="name">{card.name}</div>
+                                <div 
+                                    className="mana"
+                                    dangerouslySetInnerHTML={{ __html: replaceTextWithManaSymbols(card.mana_cost) }} 
+                                />
+                                <div className="type">{card.type}</div>
+                            </div>
+                            <div 
+                                className="oracle"
+                                dangerouslySetInnerHTML={{ __html: replaceTextWithManaSymbols(card.oracle_text) }} 
+                            />
+                        </div>
+                        <button className="action" onClick={() => {
+                                setSelectedCard(card);
+                                }}>
+                                Set As {keyword}
+                        </button>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
