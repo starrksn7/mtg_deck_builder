d8b0860327695d1fbe3a1dcae055cc590c3e6fef
starrksn7 <rpweeks_2@yahoo.com>
2026-09-23 13:08:05 -0400

    rendering the top ten list successfully

--- deck_builder_ui/src/home/home.js
+++ deck_builder_ui/src/home/home.js
@@ -1,22 +1,40 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import api from "../api/axios";
 
-export function Home(){
-    //think i'm going to use brainstorm for the name
+export function Home() {
     const [topTenCommanders, setTopTenCommanders] = useState([]);
 
     useEffect(() => {
         const getTopTenCommanders = async () => {
-            const res = await api.get('/decks/topTen');
-            
+            const res = await api.get("/decks/topTen");
             const data = res?.data;
+
             setTopTenCommanders(data);
-        }
+        };
 
         getTopTenCommanders();
-    }, [])
-    
+    }, []);
+
     return (
-        <div>Getting started on a home page</div>
-    )
+        <div>
+            <div>Getting started on a home page</div>
+
+            {topTenCommanders.map((card, i) => (
+                <div key={i}>
+                    <div className="topTenCardImage">
+                        <img
+                            src={card.imageLink}
+                            alt={card.commander}
+                        />
+                    </div>
+                    <div className="topTenCardName">
+                        {card.commander}
+                    </div>
+                    <div className="topTenCardCount">
+                        {card.count}
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
 }
